d9d599206a66f6a8afc3dfd642e293c803e9ce6e
Pichid Detson <bencomtech@gmail.com>
2018-08-27 23:14:48 +0700

    connect to firebase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, compose } from 'redux'
+import { reactReduxFirebase } from 'react-redux-firebase'
+import firebase from 'firebase'
 
 import App from './components/app'
+import firebaseConfig from './firebaseConfig'
+import rootReducer from './reducers'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+firebase.initializeApp(firebaseConfig)
+
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, { userProfile: 'users' })
+)(createStore)
+
+const initialState = {}
+const store = createStoreWithFirebase(rootReducer, initialState)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'))
 
 registerServiceWorker()
