a38ad203925526743ab66cae5c15df8232cb2f7f
Pichid Detson <bencomtech@gmail.com>
2018-08-27 22:51:50 +0700

    add firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyBoa5o26dMnYn5a5s_lvni5G8NwR9HeTmU",
+  authDomain: "todo-list-1933a.firebaseapp.com",
+  databaseURL: "https://todo-list-1933a.firebaseio.com",
+  projectId: "todo-list-1933a",
+  storageBucket: "todo-list-1933a.appspot.com",
+  messagingSenderId: "546328233812"
+}
